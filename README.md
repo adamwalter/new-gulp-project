8055f906154992535cdd7bd71df5051bb8f50bff
Adam Walter <adam@vtldesign.com>
2017-08-11 14:32:39 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# New Gulp Project Boilerplate
+
+## Includes
+
+* [Browsersync](https://www.npmjs.com/package/browser-sync)
+* [Sass](https://www.npmjs.com/package/gulp-sass)
+* [PostCSS](https://www.npmjs.com/package/gulp-postcss)
+  * [Autoprefixer](https://www.npmjs.com/package/gulp-autoprefixer)
+  * [pxtorem](https://www.npmjs.com/package/gulp-pxtorem)
+  * [CSS MQPacker](https://www.npmjs.com/package/css-mqpacker)
+  * [Unique Selectors](https://www.npmjs.com/package/postcss-unique-selectors)
+* [jshint](https://www.npmjs.com/package/gulp-jshint)
+  * [jshint-stylish](https://www.npmjs.com/package/jshint-stylish)
+* [Uglify](https://www.npmjs.com/package/gulp-uglify)
+* [Environments](https://www.npmjs.com/package/gulp-environments)
+
+## Tasks
+### Default
+`gulp`
+
+Runs all the things
+
+### Deploy
+`gulp deploy`
+
+Runs all the things plus minifies
---
+++ gulpfile.js
@@ -0,0 +1,109 @@
+'use strict';
+
+var gulp = require('gulp');
+
+var environments = require('gulp-environments');
+var development = environments.development;
+var production = environments.production;
+
+var browserSync = require('browser-sync').create();
+
+var sass = require('gulp-sass');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var pxtorem = require('postcss-pxtorem');
+var mqpacker = require('css-mqpacker');
+var uniqueSelectors = require('postcss-unique-selectors');
+
+var jshint = require('gulp-jshint');
+var stylish = require('jshint-stylish');
+var uglify = require('gulp-uglify');
+
+// Initialize Browsersync proxy
+gulp.task('browsersync', function() {
+    browserSync.init({
+        proxy: 'example.dev',
+		port: 1337
+    });
+});
+
+// Process Sass
+gulp.task('sass', function() {
+	var sassOutputStyle = production() ? 'compressed' : 'expanded';
+
+	return gulp.src('assets/sass/**/*.scss')
+		.pipe(sass({
+				outputStyle: sassOutputStyle
+			}).on('error', sass.logError))
+		.pipe(postcss([
+	        autoprefixer({
+			    expand: true,
+			    flatten: true,
+			    browsers: ['last 2 versions']
+			}),
+	        pxtorem({
+			    propWhiteList: [
+			        'font',
+			        'font-size',
+			        'line-height',
+			        'letter-spacing',
+			        'margin',
+			        'margin-top',
+			        'margin-right',
+			        'margin-bottom',
+			        'margin-left',
+			        'padding',
+			        'padding-top',
+			        'padding-right',
+			        'padding-bottom',
+			        'padding-left'
+			    ]
+			}),
+	        mqpacker({
+			    sort: true
+			}),
+	        uniqueSelectors()
+	    ]))
+		.pipe(gulp.dest('assets/css'))
+		.pipe(browserSync.stream());
+});
+
+// Lint JavaScript
+gulp.task('jshint', function() {
+	return gulp.src('assets/scripts/source/*.js')
+      .pipe(jshint())
+      .pipe(jshint.reporter(stylish))
+	  .pipe(jshint.reporter('fail'));
+});
+
+// Process JavaScript
+gulp.task('js', ['jshint'], function () {
+	return gulp.src('assets/scripts/source/*.js')
+		.pipe(production(uglify()))
+        .pipe(gulp.dest('assets/scripts/site'));
+});
+
+// Browsersync reloads for JS
+gulp.task('js-watch', ['js'], function(done) {
+	browserSync.reload();
+	done();
+});
+
+// Browsersync reloads
+gulp.task('reload-watch', function(done) {
+	browserSync.reload();
+	done();
+});
+
+// Default task
+gulp.task('default', ['browsersync'], function() {
+	gulp.watch('assets/sass/**/*.scss', ['sass']);
+	gulp.watch('assets/scripts/source/*.js', ['js-watch']);
+	gulp.watch('./*.html', ['reload-watch']);
+});
+
+// Sets production environment
+gulp.task('set-prod', production.task);
+
+// Minifies CSS and JS for production
+gulp.task('deploy', ['set-prod', 'sass', 'js']);
